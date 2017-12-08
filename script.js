daaf31670d205434ee9a95305d8e6b9992a9d8f4
Yuta K <orn_tk@me.com>
2017-12-08 21:42:17 +0900

    Adjustment for new compiler

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var xhr = new XMLHttpRequest();
 
-const language = "cpp11"
-const input = ""
-const source_code = '%23include%20%3cbits%2fstdc%2b%2b%2eh%3e%0d%0a%0d%0ausing%20namespace%20std%3b%0d%0a%0d%0aint%20main%28%29%7b%0d%0acout%20%3c%3c%20%22Hello%2cWorld%22%20%3c%3c%20endl%3b%0d%0areturn%200%3b%0d%0a%7d'
+const language = "cpp11";
+const input = "";
+const source_code = decodeURIComponent('%23include%20%3cbits%2fstdc%2b%2b%2eh%3e%0d%0a%0d%0ausing%20namespace%20std%3b%0d%0a%0d%0aint%20main%28%29%7b%0d%0acout%20%3c%3c%20%22Hello%2cWorld%22%20%3c%3c%20endl%3b%0d%0areturn%200%3b%0d%0a%7d');
 
 var reqest_time;
 
@@ -28,7 +28,7 @@ xhr.onreadystatechange = function() {
 				document.getElementById(request_queries[queries_index].selector).children[1].innerHTML = time;
 				xhr.abort();
 				setTimeout(time_req, 1000);
-				queries_index=(queries_index+1)%request_queries.length				
+				queries_index=(queries_index+1)%request_queries.length;	
             } else {
                 console.log( 'Failed. HttpStatus: '+xhr.statusText );
 				setTimeout(time_req, 10000);
@@ -38,16 +38,15 @@ xhr.onreadystatechange = function() {
 };
 
 var request_queries = [
-	{selector: "pretime", query: "language=" + language + "&source_code=" + source_code + "&input=" + input + "&precompile=true"},
-	{selector: "exetime", query: "language=" + language + "&source_code=" + source_code + "&input=" + input + ""},
-	{selector: "totaltime", query: "language=" + language + "&source_code=" + source_code + "&input=" + input + "&nocache=true"}
-]
+	{endpoint: "/build", selector: "Build", query: JSON.stringify({language: language, code: source_code})},
+	{endpoint: "/run", selector: "Run", query: JSON.stringify({language: language, code: source_code, stdin: input})},
+];
 
 var queries_index = 0;
 
 function time_req(){
 	request_time = new Date();
-	xhr.open( 'POST', 'https://compiler.ugwis.net/api/run', true );
+	xhr.open( 'POST', 'http://compiler.ugwis.net' + request_queries[queries_index].endpoint, true );
 	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhr.send(request_queries[queries_index].query);
 	console.log("time_req();");
@@ -55,4 +54,4 @@ function time_req(){
 
 window.onload = function(){
 	time_req();
-}
+};
